d550baa733a08be15d96387b69451283f962863c
mostaf1998 <me7666011@gmail.com>
2025-01-23 03:38:39 +0200

    updcbnmp

--- src/test/java/Nuca_Test/Hooks.java
+++ src/test/java/Nuca_Test/Hooks.java
@@ -32,16 +32,7 @@ public class Hooks {
         driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
         driver.navigate().to("http://10.3.1.120:7110/");
     }
-    @BeforeTest
-    public void openBrowse2() {
-        System.setProperty("webdriver.chrome.driver","D:\\Nuca_Project1\\src\\main\\resources\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
-        driver.navigate().to("http://10.3.1.120:7110/");
-    }
+
     }
 
 
